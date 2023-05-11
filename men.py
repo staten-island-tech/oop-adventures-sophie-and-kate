dbf9aaf716d8362dcde4284ce48523b2a366e0ad
kaitlynk2008 <124058190+kaitlynk2008@users.noreply.github.com>
2023-05-11 08:38:58 -0400

    Create men.py

---
+++ men.py
@@ -0,0 +1,6 @@
+class man:
+    def __init__(self,physique,personaility,Hcolor,Hstyle):
+        self.physique=physique
+        self.personaility=personaility
+        self.Hcolor=Hcolor
+        self.Hstyle=Hstyle

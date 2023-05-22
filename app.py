8de107c9e9ff67fa388502c9a73d29a021e2cd46
sophieshynder <124058239+sophieshynder@users.noreply.github.com>
2023-05-22 08:39:15 -0400

    5/22/23

--- app.py
+++ app.py
@@ -14,123 +14,131 @@ def add(x,y):
 ##class male companion
 ##class female companion
 
-
 ##desired woman class
+create_dream_woman=[]
+
 def get_female_hair(female_hair):
         question=input('choose your dream hairstyle:(a:straight, b:curly, c:layered, d:wavy, e:braids)')
         if question==('a'):
-                print('Straight=150')
+                create_dream_woman.append('Straight=150')
         if question==('b'):
-                print('Curly=70')
+                create_dream_woman.append('Curly=70')
         if question==('c'):
-                print('Layered=100')
+                create_dream_woman.append('Layered=100')
         if question==('d'):
-                print('Wavy=100')
+                create_dream_woman.append('Wavy=100')
         if question==('e'):
-                print('Braids=120')
+                create_dream_woman.append('Braids=120')
 
 
 def get_female_physique(female_physique):
         question=input('choose your dream physique:(a:curvy, b:skinny, c:muscular, d:slim thick, e:average)')
         if question==('a'):
-                print('Curvy=120')
+                create_dream_woman.append('Curvy=120')
         if question==('b'):
-                print('Skinny=100')
+                create_dream_woman.append('Skinny=100')
         if question==('c'):
-                print('Muscular=150')
+                create_dream_woman.append('Muscular=150')
         if question==('d'):
-                print('Slim Thick=200')
+                create_dream_woman.append('Slim Thick=200')
         if question==('e'):
-                print('Average=75')
+                create_dream_woman.append('Average=75')
 
 
 def get_female_personality(female_personality):
         question=input('choose your dream personality:(a:submissive, b:independent, c:baddie, d:wifey, e:angry)')
         if question==('a'):
-                print('Submissive=60')
+                create_dream_woman.append('Submissive=60')
         if question==('b'):
-                print('Independent=80 ')
+                create_dream_woman.append('Independent=80 ')
         if question==('c'):
-                print('Baddie=200')
+                create_dream_woman.append('Baddie=200')
         if question==('d'):
-                print('Wifey=215')
+                create_dream_woman.append('Wifey=215')
         if question==('e'):
-                print('Angry=20')
+                create_dream_woman.append('Angry=20')
 
 
 def get_female_hair_color(female_hair_color):
         question=input('choose your dream hair color:(a:dirty blond, b:blond, c:black, d:brunette, e:ginger)')
         if question==('a'):
-                print('Dirty Blond=50')
+                create_dream_woman.append('Dirty Blond=50')
         if question==('b'):
-                print('Blond=70')
+                create_dream_woman.append('Blond=70')
         if question==('c'):
-                print('Black=30')
+                create_dream_woman.append('Black=30')
         if question==('d'):
-                print('Brunette=70')
+                create_dream_woman.append('Brunette=70')
         if question==('e'):
-                print('Ginger=20')
+                create_dream_woman.append('Ginger=20')
+get_female_hair(1)
+get_female_physique(1)
+get_female_personality(1)
+get_female_hair_color(1)
+print(create_dream_woman)
 
 
 ##desired man person class
+create_dream_man=[]
+
 def get_male_hair(male_hair):
         question=input('choose your dream hairstyle:(a:fade, b:edgar, c:buzz, d:fluffy, e:dreads)')
         if question==('a'):
-                print('Fade=200')
+                 create_dream_man.append('Fade=200')
         if question==('b'):
-                print('Edgar=100')
+                 create_dream_man.append('Edgar=100')
         if question==('c'):
-                print('Buzz=70')
+                 create_dream_man.append('Buzz=70')
         if question==('d'):
-                print('Fluffy=80')
+                 create_dream_man.append('Fluffy=80')
         if question==('e'):
-                print('Dreads=70')
+                create_dream_man.append('Dreads=70')
 
 
 def get_male_physique(male_physique):
         question=input('choose your dream physique:(a:muscular, b:dad bod, c:skinny, d:overweight, e:body builder)')
         if question==('a'):
-                print('Muscular=200')
+                create_dream_man.append('Muscular=200')
         if question==('b'):
-                print('DadBod=40')
+                create_dream_man.append('DadBod=40')
         if question==('c'):
-                print('Skinny=150')
+                create_dream_man.append('Skinny=150')
         if question==('d'):
-                print('Overweight=20')
+                create_dream_man.append('Overweight=20')
         if question==('e'):
-                print('BodyBuilder=150')
+                create_dream_man.append('BodyBuilder=150')
 
 
 def get_male_personality(male_personality):
         question=input('choose your dream personality:(a:sigma, b:alpha, c:omega, d:npc, e:sassy)')
         if question==('a'):
-                print('Sigma=200')
+                create_dream_man.append('Sigma=200')
         if question==('b'):
-                print('Alpha=215 ')
+                create_dream_man.append('Alpha=215 ')
         if question==('c'):
-                print('Omega=50')
+                create_dream_man.append('Omega=50')
         if question==('d'):
-                print('NPC=3')
+                create_dream_man.append('NPC=3')
         if question==('e'):
-                print('Sassy=25')
+                create_dream_man.append('Sassy=25')
 
 
 def get_male_hair_color(male_hair_color):
         question=input('choose your dream hair color:(a:dirty blond, b:blond, c:black, d:brunette, e:ginger)')
         if question==('a'):
-                print('Dirty Blond=50')
+                create_dream_man.append('Dirty Blond=50')
         if question==('b'):
-                print('Blond=70')
+                create_dream_man.append('Blond=70')
         if question==('c'):
-                print('Black=30')
+                create_dream_man.append('Black=30')
         if question==('d'):
-                print('Brunette=70')
+                create_dream_man.append('Brunette=70')
         if question==('e'):
-                print('Ginger=20')
-
+                create_dream_man.append('Ginger=20')
 
 get_male_hair(1)
 get_male_physique(1)
 get_male_personality(1)
 get_male_hair_color(1)
+print(create_dream_man)
 ##compare person to pre-created companions  

d5746203f80efd3de17a7d98dc3e91a0d345ff6e
kaitlynk2008 <124058190+kaitlynk2008@users.noreply.github.com>
2023-05-22 08:04:58 -0400

    Update women.py

---
+++ women.py
@@ -0,0 +1,29 @@
+def woman1(woman1):
+    physique="muscular"
+    personaility="baddie"
+    woman_Hscolor="dirty blond"
+    woman_hair="straight"
+
+def woman2(woman2):
+    physique="curvy"
+    personaility="submissive"
+    woman_Hscolor="ginger"
+    woman_hair="braids"
+    
+def woman3(woman3):
+    physique="skinny"
+    personaility="wifey"
+    woman_Hscolor="brunette"
+    woman_hair="wavy"
+
+def woman4(woman4):
+    physique="slim thick"
+    personaility="angry"
+    woman_Hscolor="black"
+    woman_hair="layered"
+
+def woman5(woman5):
+    physique="average"
+    personaility="independant"
+    woman_Hscolor="blond"
+    woman_hair="curly"

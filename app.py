d37aed6345509cca46a44d0bc8e467acf440a6b6
kaitlynk2008 <124058190+kaitlynk2008@users.noreply.github.com>
2023-05-25 11:07:50 -0400

    kkkkk

--- app.py
+++ app.py
@@ -1,28 +1,2 @@
-class human:
-    attr1="hair color"
-    attr2="hair"
-    attr3="physique"
-    attr4="skin"
-    attr5="personaility"
 
-class Mhair:
-    Fade=200
-    Edgar=100
-    Buzz=70
-    Fluffy=80
-    Dreads=70
-
-class Fhair:
-    Straight=150
-    Curly=70
-    Layered=100
-    Wavy=100
-    Braids=120 
-
-class Mphysique:
-    Muscular=200
-    DadBod=40
-    Skinny=150
-    Overweight=20
-    BodyBuilder=150
 
